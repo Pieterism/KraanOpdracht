c5b402dbcce523b868fba70160c5e74de18e4966
Pieterism <pervdh@gmail.com>
2018-11-06 14:54:56 +0100

    init IntelliJ

---
+++ KraanOpdracht/src/Item.java
@@ -0,0 +1,17 @@
+
+
+/**
+ * Created by Wim on 12/05/2015.
+ */
+public class Item {
+
+    private final int id;
+
+    public Item(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
---
+++ KraanOpdracht/src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+/*        try {
+            //Problem p=Problem.fromJson(new File("1_1_3_3_FALSE_100_14_14.json"));
+
+
+            Problem p = Problem.fromJson(new File("1_50_50_10_FALSE_60_25_100.json"));
+
+            p.prepareSolve();
+            p.solve();
+
+
+        } catch (IOException | ParseException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }*/
+
+
+    }
+
+}
